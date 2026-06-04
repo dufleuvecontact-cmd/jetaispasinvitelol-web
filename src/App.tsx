542caa9780edf101beea9a3fc97c1a735e0127d4
Jad G <dufleuvecontact@gmail.com>
2026-06-04 02:22:42 -0400

    Remove settings icon

--- src/App.tsx
+++ src/App.tsx
@@ -30,9 +30,7 @@ const MailIcon = () => (
   <svg viewBox="0 0 24 24"><path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z"/></svg>
 );
 
-const GearIcon = () => (
-  <svg viewBox="0 0 24 24"><path d="M19.14 12.94c.04-.3.06-.61.06-.94 0-.32-.02-.64-.07-.94l2.03-1.58a.49.49 0 0 0 .12-.61l-1.92-3.32a.49.49 0 0 0-.59-.22l-2.39.96c-.5-.38-1.03-.7-1.62-.94l-.36-2.54a.484.484 0 0 0-.48-.41h-3.84c-.24 0-.43.17-.47.41l-.36 2.54c-.59.24-1.13.57-1.62.94l-2.39-.96a.49.49 0 0 0-.59.22L2.74 8.87c-.12.21-.08.47.12.61l2.03 1.58c-.05.3-.07.62-.07.94s.02.64.07.94l-2.03 1.58a.49.49 0 0 0-.12.61l1.92 3.32c.12.22.37.29.59.22l2.39-.96c.5.38 1.03.7 1.62.94l.36 2.54c.05.24.24.41.48.41h3.84c.24 0 .44-.17.47-.41l.36-2.54c.59-.24 1.13-.56 1.62-.94l2.39.96c.22.08.47 0 .59-.22l1.92-3.32c.12-.22.07-.47-.12-.61l-2.01-1.58zM12 15.6A3.6 3.6 0 1 1 12 8.4a3.6 3.6 0 0 1 0 7.2z"/></svg>
-);
+
 
 const QuestionIcon = () => (
   <svg viewBox="0 0 24 24"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 16h-2v-2h2v2zm1.07-7.75l-.9.92C12.45 11.9 12 12.5 12 14h-2v-.5c0-1.1.45-2.1 1.17-2.83l1.24-1.26c.37-.36.59-.86.59-1.41 0-1.1-.9-2-2-2s-2 .9-2 2H7c0-2.76 2.24-5 5-5s5 2.24 5 5c0 1.04-.42 1.99-1.07 2.75z"/></svg>
@@ -308,7 +306,6 @@ export default function App() {
               <div className="nav-icon-link" title={t.dashboard} onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}><HomeIcon /></div>
               <a href="https://instagram.com/jetaispasinvite" target="_blank" rel="noreferrer" className="nav-icon-link" title="Instagram" style={{ color: "inherit" }}><MailIcon /></a>
               <Link to="/terms" className="nav-icon-link" title="Privacy & TOS" style={{ color: "inherit" }}><QuestionIcon /></Link>
-              <div className="nav-icon-link" title={t.settings}><GearIcon /></div>
             </div>
           </div>
         </div>
